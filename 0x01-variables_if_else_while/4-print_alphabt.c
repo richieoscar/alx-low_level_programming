ff3e614d15e1e8638906a61c69f7638937768bf0
richieoscar <oscaranyiam94@gmail.com>
2022-09-11 15:07:36 -0700

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,13 @@
 int main(void)
 {
 	char l_letter = 'a';
+
 	while (l_letter <= 'z')
 	{
 		if (!(l_letter == 'e' || l_letter == 'q'))
 		{
 			putchar(l_letter);
 		}
-
 		l_letter++;
 	}
 	putchar('\n');
